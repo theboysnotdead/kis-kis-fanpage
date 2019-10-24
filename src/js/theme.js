a1de44ebba39f541e275328ed8a1f0d4bad62228
nazarukroman <nazarukroman@bk.ru>
2019-10-24 19:59:44 +0300

    set dark theme to default

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,16 +1,34 @@
-const html = document.querySelector("html");
+const TOGGLER_ID = {
+  WHITE: 'white-toggler',
+  DARK: 'dark-toggler'
+}
 
-document.querySelectorAll('input[name="theme-type"]').forEach(toggler =>
+const html = document.querySelector("html");
+const themeTogglers = document.querySelectorAll('input[name="theme-type"]');
+
+document.querySelectorAll('input[name="theme-type"]').forEach(toggler => {
+  if (
+    toggler.id === TOGGLER_ID.WHITE &&
+    html.classList.contains("is-dark")
+  ) {
+    html.classList.remove("is-dark");
+  }
+
+
+  if (toggler.id === TOGGLER_ID.DARK) {
+    html.classList.add("is-dark");
+  }
+
   toggler.addEventListener("click", ({ currentTarget }) => {
     if (
-      currentTarget.id === "white-toggler" &&
+      currentTarget.id === TOGGLER_ID.WHITE &&
       html.classList.contains("is-dark")
     ) {
       html.classList.remove("is-dark");
     }
 
-    if (currentTarget.id === "dark-toggler") {
+    if (currentTarget.id === TOGGLER_ID.DARK) {
       html.classList.add("is-dark");
     }
   })
-);
+});
